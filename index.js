6eef622890b1d190b2f928b233e1c2214af7b539
Htoo Wai Yan <htoowaiyan.dev@gmail.com>
2024-01-25 23:01:57 +0630

    change db user in index.js

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 const uri =
-  "mongodb+srv://<user>:<password>@awesomeres.0oaokrz.mongodb.net/awesomerestaurant?retryWrites=true&w=majority";
+  "mongodb+srv://dev:<password>@awesomeres.0oaokrz.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(uri.replace(
-    "<user>", process.env.DBUSER,
     "<password>", process.env.DBPASSWORD))
   .then((conn) => {
     // console.log(conn);
