c047ea396fc1ac91c7d0411b52aa414c2bc27698
Htoo Wai Yan <htoowaiyan.dev@gmail.com>
2024-01-25 22:48:57 +0630

    added cors
    use cors * to be called from anywhere

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const logger = require("./middlewares/logger");
 const dotenv = require("dotenv");
 const globalErrorHandler = require("./controllers/errorCtrl");
 const AppError = require("./api_features/appError");
+const cors = require("cors");
 
 dotenv.config({ path: "./config.env" });
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({ origin: '*' }));
 app.use(logger.myLogger);
-app.use(logger.reqTimeLog);
 
 if (process.env.NODE_ENVIRONMENT == "development") {
   app.use(morgan("dev"));
